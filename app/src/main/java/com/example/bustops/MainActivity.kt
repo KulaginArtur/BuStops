b7930d5c4de186b3a12d3b19b47c79c9964819b1
svidhaji <svidhaji@gmail.com>
2020-02-26 12:44:54 +0200

    manifest change

--- app/src/main/java/com/example/bustops/MainActivity.kt
+++ app/src/main/java/com/example/bustops/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.activity_main.*
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -16,6 +17,7 @@ import org.osmdroid.util.GeoPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    var currentL = LatLng(60.17, 24.95)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,11 +47,9 @@ class MainActivity : AppCompatActivity() {
                 val msg = "latitude: ${task.result!!.latitude} and longitude: ${task.result!!.longitude}"
                 main_textview.text = msg
                 map.setTileSource(TileSourceFactory.MAPNIK)
-                //map.setBuiltInZoomControls(true)
                 map.setMultiTouchControls(true)
                 map.controller.setZoom(17.0)
                 map.controller.setCenter(GeoPoint(task.result!!.latitude, task.result!!.longitude))
-
             }
         }
 
