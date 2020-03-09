d6a3f29a8be52b836114763bcddcf7a1196fa957
svidhaji <svidhaji@gmail.com>
2020-03-09 12:58:10 +0200

    Location markers added to nearby stops

--- app/src/main/java/com/example/bustops/MainActivity.kt
+++ app/src/main/java/com/example/bustops/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.bustops
 
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.location.Geocoder
+import android.location.Location
+import android.location.Address
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentTransaction
 import com.example.bustops.model.StopPost
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -22,25 +36,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var homeFragment: HomeFragment
     lateinit var arFragment: ArFragment
     lateinit var settingsFragment: SettingsFragment
-
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val REQUEST_CHECK_SETTINGS = 3
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        homeFragment = HomeFragment()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.frame_layout, homeFragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .commit()
-
         btm_nav.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
 
                 R.id.home -> {
-                    homeFragment = HomeFragment()
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.frame_layout, homeFragment)
@@ -69,16 +85,173 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             true
         }
 
-        //fetchJson()
+        fetchJson()
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                Log.d("Locationupdate", "${p0.lastLocation}")
+                //placeMarkerOnMap(p0.lastLocation)
+                Log.d("Locationupdate", "still alive?")
+                //addMarkers(p0.lastLocation)//LatLng(lastLocation.latitude, lastLocation.longitude))
+                Log.d("Locationupdate", "why place marker is no called?")
+
+            }
+        }
+        createLocationRequest()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                locationUpdateState = true
+                startLocationUpdates()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (!locationUpdateState) {
+            startLocationUpdates()
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        setUpMap()
     }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+
+        mMap.isMyLocationEnabled = true
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        mMap.isBuildingsEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                //placeMarkerOnMap(currentLatLng)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 16f))
+            }
+        }
+    }
+
+    /* private fun placeMarkerOnMap(location: LatLng) {
+        val markerOptions = MarkerOptions().position(location)
+            .title(getAddress(location))
+        /*markerOptions.icon(BitmapDescriptorFactory.fromBitmap(
+            BitmapFactory.decodeResource(resources, R.mipmap.ic_user_location)))*/
+        mMap.addMarker(markerOptions)
+    }*/
+
+
+    private fun getAddress(latLng: LatLng): String {
+        // 1
+        val geocoder = Geocoder(this)
+        val addresses: List<Address>?
+        val address: Address
+        var addressText = ""
+
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            if (null != addresses && addresses.isNotEmpty()) {
+                address = addresses[0]
+                for (i in 0 until address.maxAddressLineIndex) {
+                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(
+                        i
+                    )
+                }
+            }
+        } catch (e: IOException) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+
+        return addressText
+    }
+
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10 * 10000
+        locationRequest.fastestInterval = 3 * 30000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdates()
+        }
+        task.addOnFailureListener { e ->
+            if (e is ResolvableApiException) {
+                // Check your location settings first, if location is enabled
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    e.startResolutionForResult(
+                        this@MainActivity,
+                        REQUEST_CHECK_SETTINGS
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Catches if there's any errors and ignores it
+                }
+            }
+        }
+    }
+
 
     private fun fetchJson() {
         val url =
@@ -96,8 +269,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 val body = response.body?.string()
                 println(body)
                 val gson = GsonBuilder().create()
-                val stop = gson.fromJson(body, Stops::class.java)
-                callWebService(stop)
+                val stop = gson.fromJson(body, Stops2::class.java)
+
+                val stop1  = Stops2( "Leiritie", 60.258942,24.846895)
+                val stop2  = Stops2( "Leiritie",  60.25919,24.84605)
+                val stop3  = Stops2( "Honkasuo", 60.258935,24.843145)
+                val stop4  = Stops2( "Raappavuorentie", 60.25945,24.84224)
+
+                runOnUiThread(){
+                    addMarkers2(stop1)
+                    addMarkers2(stop2)
+                    addMarkers2(stop3)
+                    addMarkers2(stop4)
+                }
+
+                //callWebService(stop)
             }
 
             override fun onFailure(call: Call, e: IOException) {
@@ -167,10 +353,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     fun addMarkers(stop: Stops) {
         for (i in stop.features) {
             if (i.attributes.REI_VOIM == 1 && i.attributes.AIK_VOIM == 1) {
-                val loc = LatLng(i.geometry.y, i.geometry.x)
+                println("found name: ${i.attributes.NIMI1}")
+                val loc = LatLng(i.geometry.x, i.geometry.y)
                 mMap.addMarker(
                     MarkerOptions().position(loc).title(i.attributes.SOLMUTUNNU).snippet(
-                        i.attributes.REI_VOIM.toString()
+                        i.attributes.NIMI1
                     )
                 )
             }
@@ -178,18 +365,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     fun addMarkers2(stop: Stops2) {
-        val loc = LatLng(stop.lat, stop.lon)
-        mMap.addMarker(MarkerOptions().position(loc).title(stop.name))
+        val loc = LatLng(stop.x, stop.y)
+        mMap.addMarker(
+            MarkerOptions().position(loc).title(stop.NIMI1)
+        )
     }
+
+
+    class Stops(val features: List<Stop>)
+    class Stop(val attributes: Attributes, val geometry: Geometry)
+    class Attributes(
+        val SOLMUTUNNU: String,
+        val NIMI1: String,
+        val REI_VOIM: Int,
+        val AIK_VOIM: Int
+    )
+
+    class Geometry(val x: Double, val y: Double)
+
+    class Stops2(
+        val NIMI1: String,
+        val x: Double,
+        val y: Double
+    )
 }
-
-class Stops(val features: List<Stop>)
-class Stop(val attributes: Attributes, val geometry: Geometry)
-class Attributes(val SOLMUTUNNU: String, val NIMI1: String, val REI_VOIM: Int, val AIK_VOIM: Int)
-class Geometry(val x: Double, val y: Double)
-
-class Stops2(
-    val name: String,
-    val lat: Double,
-    val lon: Double
-)
